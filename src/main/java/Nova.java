ed10547a8fcf4c845da467068b13febef9595b05
Yash Thakker <87691410+Yash-PoPularPlusPlus@users.noreply.github.com>
2026-08-21 01:10:53 +0800

    Implement Level-4 task types

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -4,7 +4,9 @@ public class Nova {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        String[] descriptions = new String[100];
+        String[] types = new String[100];
+        String[] extraInfo = new String[100];
         boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
@@ -16,24 +18,90 @@ public class Nova {
 
             if (input.equals("bye")) {
                 break;
+
             } else if (input.equals("list")) {
                 for (int i = 0; i < taskCount; i++) {
                     String status = isDone[i] ? "X" : " ";
-                    System.out.println((i + 1) + ".[" + status + "] " + tasks[i]);
+
+                    if (types[i].equals("T")) {
+                        System.out.println(
+                                (i + 1) + ".[T][" + status + "] " + descriptions[i]
+                        );
+                    } else if (types[i].equals("D")) {
+                        System.out.println(
+                                (i + 1) + ".[D][" + status + "] "
+                                        + descriptions[i]
+                                        + " (by: " + extraInfo[i] + ")"
+                        );
+                    } else if (types[i].equals("E")) {
+                        System.out.println(
+                                (i + 1) + ".[E][" + status + "] "
+                                        + descriptions[i]
+                                        + " " + extraInfo[i]
+                        );
+                    }
                 }
+
             } else if (input.startsWith("mark ")) {
                 int taskNumber = Integer.parseInt(input.substring(5));
                 int index = taskNumber - 1;
-
                 isDone[index] = true;
 
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + tasks[index]);
-            } else {
-                tasks[taskCount] = input;
+                System.out.println("Nice! I've marked this task as done.");
+
+            } else if (input.startsWith("todo ")) {
+                descriptions[taskCount] = input.substring(5);
+                types[taskCount] = "T";
+                extraInfo[taskCount] = "";
                 isDone[taskCount] = false;
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println("[T][ ] " + descriptions[taskCount]);
+
                 taskCount++;
-                System.out.println("added: " + input);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+            } else if (input.startsWith("deadline ")) {
+                String content = input.substring(9);
+                String[] parts = content.split(" /by ", 2);
+
+                descriptions[taskCount] = parts[0];
+                types[taskCount] = "D";
+                extraInfo[taskCount] = parts[1];
+                isDone[taskCount] = false;
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println(
+                        "[D][ ] " + descriptions[taskCount]
+                                + " (by: " + extraInfo[taskCount] + ")"
+                );
+
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+            } else if (input.startsWith("event ")) {
+                String content = input.substring(6);
+
+                String[] fromParts = content.split(" /from ", 2);
+                String description = fromParts[0];
+
+                String[] toParts = fromParts[1].split(" /to ", 2);
+                String from = toParts[0];
+                String to = toParts[1];
+
+                descriptions[taskCount] = description;
+                types[taskCount] = "E";
+                extraInfo[taskCount] = "(from: " + from + " to: " + to + ")";
+                isDone[taskCount] = false;
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println(
+                        "[E][ ] " + descriptions[taskCount]
+                                + " " + extraInfo[taskCount]
+                );
+
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
             }
         }
 
