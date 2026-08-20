1fb50692dec025991ea912013a3f56dbaad3a536
Yash Thakker <87691410+Yash-PoPularPlusPlus@users.noreply.github.com>
2026-08-21 00:34:40 +0800

    Implement Level-1 command echo

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Nova {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Nova.");
         System.out.println("What can I do for you?");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
