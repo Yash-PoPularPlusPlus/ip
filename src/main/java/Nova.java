68c721bca8f141d6a9684896e517c7f349429370
Yash Thakker <87691410+Yash-PoPularPlusPlus@users.noreply.github.com>
2026-08-21 00:38:08 +0800

    Implement Level-2 task storage and listing

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -4,6 +4,9 @@ public class Nova {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello! I'm Nova.");
         System.out.println("What can I do for you?");
 
@@ -12,9 +15,15 @@ public class Nova {
 
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
-
-            System.out.println(input);
         }
 
         System.out.println("Bye. Hope to see you again soon!");
