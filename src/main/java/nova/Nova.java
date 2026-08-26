c3804934862e3b17bb7d651a37a17674f760173f
Yash Thakker <87691410+Yash-PoPularPlusPlus@users.noreply.github.com>
2026-08-26 19:23:40 +0800

    Organize classes into nova package
    Move all application classes under the nova package while keeping src/main/java as the source root.

--- src/main/java/nova/Nova.java
+++ src/main/java/nova/Nova.java
@@ -1,3 +1,5 @@
+package nova;
+
 import java.io.IOException;
 import java.nio.file.Path;
 
--- src/main/java/nova/TaskList.java
+++ src/main/java/nova/TaskList.java
@@ -1,3 +1,5 @@
+package nova;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
